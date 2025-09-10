d7d7a1c9b31669f82bb2f68c4fcec2bbf5aab699
Martin Sigloch <f0i.git@projects.f0i.de>
2025-09-10 19:26:33 +0000

    remove: gradient overlay from leaderboard
    - Remove unnecessary gradient overlay element
    - Clean up leaderboard component styling
    - Remove absolute positioned gradient div

--- src/poi_frontend/src/components/Leaderboard.jsx
+++ src/poi_frontend/src/components/Leaderboard.jsx
@@ -373,8 +373,7 @@ function Leaderboard() {
                   })}
                 </div>
 
-                {/* Gradient overlay for smooth blur transition */}
-                <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-slate-900 via-slate-900/80 to-transparent pointer-events-none"></div>
+
               </div>
             )}
           </div>
